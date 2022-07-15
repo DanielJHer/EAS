ca3d90024bfa78b7618e30f638ad748e8625bc55
Daniel Her <danieljhher@gmail.com>
2022-07-14 22:04:09 -0700

    Added function that prompts and creates grid.

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-// function that hovers over
-// dom for each div
 
-// creates grid 
-const div = document.createElement('div')
-document.getElementById('main').appendChild(div);
+// function that creates grid 
+const container = document.getElementById('container');
 
-function createGrid(x) {
-    for(let i=0; i > x; i++) {
-        document.getElementById('main').appendChild(div);
+createGrid = (y) => {
+    for(let i=0; i<y; i++) {
+        let row = document.createElement('div');
+        row.className= 'row';
+        container.appendChild(row);
+        
+        for(let j=0; j<y; j++) {
+            let box =document.createElement('div');
+            box.className= 'box';
+            row.appendChild(box);
+
+            // changes color of cells when hovering
+            box.addEventListener('mouseover', function(event){
+                event.target.style.backgroundColor = 'black';
+            })
+        }
     }
 }
 
-// accepts number of rows and columns 
-createGrid();
+// button which prompts for number of rows and columns 
+const button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+    const promptAnswer = prompt('How many squares per side would you like?') 
+    const y = parseInt(promptAnswer);
+
+    if (y == null || y == '') {
+        alert('User cancelled prompt');
+    } else if(y<0 || y>101) {
+        alert('Pick a number between 1-100!')
+    }
+    createGrid(y);
+})
